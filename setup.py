edc7f6cf3f39da18833923daca4dac23e33a1f6b
Colin O'Flynn <coflynn@newae.com>
2024-03-20 17:19:58 -0300

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ from setuptools import setup
 
 setup(
     name = 'pyjtagbs',
-    version = '1.0.0',
+    version = '1.0.1',
     description = "JTAG with the BS (Boundary Scan)",
     author = "Colin O'Flynn",
-    author_email = 'coflynn@newae.com',
+    author_email = 'colin@oflynn.com',
     license = 'LGPLv2.1',
-    url = 'http://www.newae.com',
+    url = 'http://www.colinoflynn.com',
     download_url='https://github.com/colinoflynn/pyjtagbs',
-    packages = ['jtagbs',
-                ],
+    packages = [find_packages()],
     install_requires = [
     ],
 )
